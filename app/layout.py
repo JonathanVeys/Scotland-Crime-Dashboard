e7a9efbf355a5d29614eeb09c7a81fd8e6874c70
Jonathan Veys <Jonathancr1234@gmail.com>
2025-09-06 01:08:29 +0100

    Began design on front end, got a basic layout and placed a temporary choropleth map as a placeholder

--- app/layout.py
+++ app/layout.py
@@ -1,46 +1,73 @@
 from dash import dcc, html
+import plotly.express as px
+
+df = px.data.gapminder().query("year == 2007")
+
+# Create a choropleth (world map)
+fig = px.choropleth(
+    df,
+    locations="iso_alpha",    # country codes
+    color="lifeExp",          # color by life expectancy
+    hover_name="country",     # hover info
+    color_continuous_scale=px.colors.sequential.Plasma,
+    projection='natural earth'
+)
+
+fig.update_layout(coloraxis_showscale=False)
+fig.update_layout(
+    paper_bgcolor="#303030",
+    geo=dict(
+        showframe=False,        # remove frame around map
+        showcoastlines=False,   # remove coastlines
+        bgcolor='#303030',      # background inside map area
+        landcolor="#202020",    # land color
+        lakecolor="#303030",    # lakes color
+        oceancolor="#303030"    # oceans color
+    )
+)
+fig.update_layout(
+    margin=dict(l=0, r=0, t=0, b=0)  # left, right, top, bottom
+)
 
 layout = html.Div([
     html.Div([
-        dcc.Slider(
-            id='monthly_slider',
-            marks={
-                1:'Jan', 2:'Feb', 3:'Mar',
-                4:'Apr', 5:'May', 6:'Jun',
-                7:'Jul', 8:'Aug', 9:'Sep',
-                10:'Oct', 11:'Nov', 12:'Dec'},
-                step=1
-        )
-    ], className='input-box'),
+        html.Div([
+            html.Div([
+                html.H3('Crime Breakdown')
+            ], className='title-container'),
+
+            html.Div([
+
+            ], className='content-container')
+        ], className='crime-graph-container', style={'margin-right':'5px'}),
+
+        html.Div([
+            html.Div([
+                html.H3('Map')
+            ], className='title-container'),
+            html.Div([
+                dcc.Graph(figure=fig, style={"height": "100%", "width": "100%"}, config={"displayModeBar": False})
+            ], className='content-container')
+        ], className='scotland-map-container', style={'margin-left':'5px'})
+    ], className='container', style={'flex':'2'}),
 
     html.Div([
         html.Div([
-            dcc.Graph(
-                id='crime_map',
-                figure = {}, 
-                className='scotland-map',
-                config = {'displayModeBar': False}
-                )
-        ], className='choropleth-map-div'),
+
+        ], className='pie-chart-container', style={'margin-left':'10px', 'margin-right':'5px'}),
+
+        html.Div([  
+
+        ], className='pie-chart-container', style={'margin-left':'5px', 'margin-right':'5px'}),
+
         html.Div([
-            html.Div([
-                dcc.Graph(
-                    id='crime_breakdown_pie',
-                    figure={},
-                    # className='pie-chart',
-                    config={'displayModeBar': False}
-                    )
-            ], className='crime-breakdown-div'),
-            html.Div([
-                dcc.Graph(
-                    id='council_crime_timeseries',
-                    figure={},
-                    # className='timeseries-graph',
-                    config={'displayModeBar': False}
-                    )
-            ], className='timeseries-div')
-        ], className='graphs-div')
-    ], className='output-box')
+
+        ], className='pie-chart-container', style={'margin-left':'5px', 'margin-right':'10px'})
+    ], className='container', style={'flex':'1'}),
+
+    html.Div([
+        html.Div([
+
+        ], className='data-container')
+    ], className='container', style={'flex':'1'})
 ], className='main-container')
-
-
